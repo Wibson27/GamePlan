f92f8d0125e2dac21cfc1c3f39a3bac606dae34f
RIFQI WIBISONO <rifqiwibisono271002@gmail.com>
2025-06-20 21:56:33 +0700

    Images Placeholder

---
+++ target/debug/deps/backend-c2a1917ab6035e1e.d
@@ -0,0 +1,5 @@
+/home/rifqi/PROJECTS/GamePlan/target/debug/deps/libbackend-c2a1917ab6035e1e.rmeta: backend/src/lib.rs
+
+/home/rifqi/PROJECTS/GamePlan/target/debug/deps/backend-c2a1917ab6035e1e.d: backend/src/lib.rs
+
+backend/src/lib.rs:
